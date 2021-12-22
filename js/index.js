f6ef6efe0a40e610b88645d0b5d2c2c9525fca2a
AytacMursalzade <93290350+AytacMursalzade@users.noreply.github.com>
2021-12-22 04:02:53 +0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,48 @@
+
+let background;
+
+
+function myFunction(a){
+    background = setTimeout(alertFunc, );
+}
+function alertFunc(a){
+    alert("WIN!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function changeBg(a){
+
+    if(a.style.background == "green" && a.style.borderRadius == "60%"){
+       a.style.borderRadius = "60%";
+       a.style.background = "green" 
+    }else{
+        a.style.background = "yellow"
+    }
+}
+
+
+function changeBgClick(a){
+
+    if(a.style.background == "red" && a.style.borderRadius == "60%"){
+        a.style.borderRadius = "60%";
+        a.style.background = "red" 
+    }else{
+        a.style.background = "green"
+    }
+}
+
+function changeBgOut(a){
+   
+}
+
+
